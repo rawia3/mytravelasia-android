7998733d257b3962e521318e564a626a3ffc65d2
Evan Dale Aromin <avendael@gmail.com>
2013-10-18 23:47:33 +0800

    #1: Rudimentary list item layout for the main list feed.

---
+++ src/com/r2mobile/mytravelasia/MainActivity.java
@@ -0,0 +1,19 @@
+package com.r2mobile.mytravelasia;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * An activity that manages the feed and map fragments. If the device has a smallest width of >= 530dp, it will show
+ * the feed list fragment beside the map fragment. Otherwise, they will be shown sequentially, with the feed list
+ * fragment taking precedence.
+ *
+ * @author avendael
+ */
+public class MainActivity extends FragmentActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
