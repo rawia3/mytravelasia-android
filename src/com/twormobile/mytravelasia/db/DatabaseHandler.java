3d6e88f0e899eef9e026d1003bcad87e887fb8ea
Evan Dale Aromin <avendael@gmail.com>
2013-10-21 23:04:15 +0800

    Issue #1: Database handler for creating the local DB that will store the POI cache.

---
+++ src/com/twormobile/mytravelasia/db/DatabaseHandler.java
@@ -0,0 +1,43 @@
+package com.twormobile.mytravelasia.db;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.twormobile.mytravelasia.model.Poi;
+
+/**
+ * Helper for creating the database and its tables.
+ *
+ * @author avendael
+ */
+public class DatabaseHandler extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "mytravelasia-ph";
+
+    public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                           int version) {
+        super(context, name, factory, version);
+    }
+
+    public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                           int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Poi.SQL_CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(Poi.SQL_DROP_TABLE);
+
+        db.execSQL(Poi.SQL_CREATE_TABLE);
+    }
+}
