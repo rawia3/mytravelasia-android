e3a8325aa534fc3cdb121e9a36beb85e7cc7f0f7
Evan Dale Aromin <avendael@gmail.com>
2013-10-26 18:00:31 +0800

    Issue #1: Skeleton intent service to handle calling the webservice and save the results to the provider.

--- src/com/twormobile/mytravelasia/StartUpActivity.java
+++ src/com/twormobile/mytravelasia/StartUpActivity.java
@@ -1,7 +1,6 @@
 package com.twormobile.mytravelasia;
 
 import android.app.Activity;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
@@ -10,12 +9,8 @@ import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
-import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.twormobile.mytravelasia.db.MtaPhProvider;
-import com.twormobile.mytravelasia.http.FeedHttpClient;
-import com.twormobile.mytravelasia.http.FeedResponse;
-import com.twormobile.mytravelasia.model.Poi;
-import com.twormobile.mytravelasia.util.Log;
+import com.twormobile.mytravelasia.http.FeedIntentService;
 
 /**
  * An activity which serves as the startup entry point of the app. It shows an overview about Philippines, and some ads at
@@ -47,44 +42,15 @@ public class StartUpActivity extends Activity {
 
         initAds();
         getContentResolver().delete(MtaPhProvider.POI_URI, null, null);
-
-        FeedHttpClient.getFeeds(new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(String content) {
-                Log.d(TAG, "response is " + content);
-                // TODO: Move the getFeed call to a Service or IntentService
-                FeedResponse feedResponse = FeedHttpClient.getFeedGsonParser().fromJson(content, FeedResponse.class);
-
-                for (Poi poi : feedResponse.getFeeds()) {
-                    Log.d(TAG, "poi name: " + poi.getName());
-                    ContentValues values = new ContentValues();
-
-                    values.put(Poi.RESOURCE_ID, poi.getResourceId());
-                    values.put(Poi.NAME, poi.getName());
-                    values.put(Poi.ADDRESS, poi.getAddress());
-                    values.put(Poi.CONTENT, poi.getContent());
-                    values.put(Poi.FB_USER_PROFILE_ID, poi.getFbUserProfileId());
-                    values.put(Poi.FB_USER_PROFILE_NAME, poi.getFbUserProfileName());
-                    values.put(Poi.CREATED_AT, poi.getCreatedAt());
-                    values.put(Poi.LONGITUDE, poi.getLongitude());
-                    values.put(Poi.LATITUDE, poi.getLatitude());
-                    values.put(Poi.FEED_TYPE, poi.getFeedType());
-                    values.put(Poi.POI_TYPE, poi.getPoiType());
-                    values.put(Poi.IMAGE_THUMB_URL, poi.getImageThumbUrl());
-                    values.put(Poi.ANNOTATION_TYPE, poi.getAnnotationType());
-                    values.put(Poi.TOTAL_COMMENTS, poi.getTotalComments());
-                    values.put(Poi.TOTAL_LIKES, poi.getTotalLikes());
-
-                    getContentResolver().insert(MtaPhProvider.POI_URI, values);
-                }
-            }
-        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mWvAds.loadUrl(ADS_URL);
+
+        Intent getFeedIntent = new Intent(this, FeedIntentService.class);
+        startService(getFeedIntent);
     }
 
     private void initAds() {
