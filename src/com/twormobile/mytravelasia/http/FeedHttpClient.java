4df3beb2bb6347e43b2c0d3ce262711cbdf165e3
Evan Dale Aromin <avendael@gmail.com>
2013-10-26 18:18:01 +0800

    Issue #1: AsyncHttpClient doesn't work inside an IntentService. Changed FeedHttpClient to use SyncHttpClient instead.

--- src/com/twormobile/mytravelasia/http/FeedHttpClient.java
+++ src/com/twormobile/mytravelasia/http/FeedHttpClient.java
@@ -3,9 +3,9 @@ package com.twormobile.mytravelasia.http;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.SyncHttpClient;
 
 /**
  * Retrieves feeds from the REST webservice.
@@ -15,7 +15,12 @@ import com.loopj.android.http.RequestParams;
 public class FeedHttpClient {
     private static final String BASE_URL = "http://www.mytravel-asia.com/";
 
-    private static AsyncHttpClient client = new AsyncHttpClient();
+    private static SyncHttpClient client = new SyncHttpClient() {
+        @Override
+        public String onRequestFailed(Throwable error, String content) {
+            return content;
+        }
+    };
 
     public static void getFeeds(AsyncHttpResponseHandler responseHandler) {
         RequestParams params = new RequestParams();
