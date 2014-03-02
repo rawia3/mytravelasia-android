1e5a6d7cca91a35c440630b5c20a1e70ca16b1b2
Evan Dale Aromin <avendael@gmail.com>
2014-03-02 20:23:47 +0800

    #14: Show the profile photos of the commenters.

--- src/com/twormobile/mytravelasia/philippines/feed/CommentsArrayAdapter.java
+++ src/com/twormobile/mytravelasia/philippines/feed/CommentsArrayAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import com.loopj.android.image.SmartImageView;
 import com.twormobile.mytravelasia.philippines.R;
 import com.twormobile.mytravelasia.philippines.model.CommentEntry;
 
@@ -20,6 +21,7 @@ import java.util.List;
 public class CommentsArrayAdapter extends ArrayAdapter<CommentEntry> {
     static class ViewHolder {
         TextView tvContent;
+        SmartImageView ivProfilePic;
     }
 
     public CommentsArrayAdapter(Context context, int resource, List<CommentEntry> objects) {
@@ -35,6 +37,7 @@ public class CommentsArrayAdapter extends ArrayAdapter<CommentEntry> {
             LayoutInflater inflater = ((Activity) getContext()).getLayoutInflater();
             view = inflater.inflate(R.layout.comment_list_item, parent, false);
             viewHolder.tvContent = (TextView) view.findViewById(R.id.tv_content);
+            viewHolder.ivProfilePic = (SmartImageView) view.findViewById(R.id.iv_profile_pic);
 
             view.setTag(viewHolder);
         } else {
@@ -42,8 +45,12 @@ public class CommentsArrayAdapter extends ArrayAdapter<CommentEntry> {
         }
 
 
-        String content = getItem(position).getContent();
+        CommentEntry commentEntry = getItem(position);
+        String content = commentEntry.getContent();
+        String photoUrl = "http://graph.facebook.com/" + commentEntry.getFbUserProfileId() + "/picture";
+
         viewHolder.tvContent.setText(content);
+        viewHolder.ivProfilePic.setImageUrl(photoUrl);
 
         return view;
     }
