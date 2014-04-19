df8c8a20b21ff140498118029a3eca05afb8487d
rupert <rupert@2rmobile.com>
2014-04-20 07:32:16 +1000

    Fix #27 to maintain picture aspect ratio during full screen using "fitCenter".  See http://developer.android.com/reference/android/widget/ImageView.ScaleType.html

--- src/com/twormobile/mytravelasia/philippines/feed/PoiPhotoActivity.java
+++ src/com/twormobile/mytravelasia/philippines/feed/PoiPhotoActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.loopj.android.image.SmartImageView;
 import com.twormobile.mytravelasia.philippines.R;
 import com.twormobile.mytravelasia.philippines.util.AppConstants;
+import com.twormobile.mytravelasia.philippines.util.Log;
 
 /**
  * An activity which shows a POI photo in full screen.
@@ -20,6 +21,7 @@ public class PoiPhotoActivity extends Activity {
 
         String photoUrl = getIntent().getStringExtra(AppConstants.ARG_PHOTO_URL);
         SmartImageView ivPhoto = (SmartImageView) findViewById(R.id.iv_photo);
+        Log.d(TAG, photoUrl);
 
         if (photoUrl != null) ivPhoto.setImageUrl(photoUrl);
     }
